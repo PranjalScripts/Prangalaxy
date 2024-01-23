aacfbff1b71e0e85edaaa7936532a270979f0bb1
Pranjal Tiwari <pranjalt363@gmail.com>
2024-01-24 00:40:08 +0530

    final update 24/01/24

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";  
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Configuration of env
 dotenv.config();
 // Connect to database
@@ -17,16 +23,19 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.static(path.join(__dirname,'./prangalaxy/build')));
 // Routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoute);
 app.use('/api/v1/product', productRoute);
-// Rest API
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./prangalaxy/build/index.html'));
+});
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to PranGalaxy Website</h1>');
 });
 
-const port = process.env.PORT || 5000;  
+const port = process.env.port || 5000;  
 // server Run
 app.listen(port, () =>
   console.log(`Server running on ${process.env.msg} port ${port}`.bgCyan.white)
